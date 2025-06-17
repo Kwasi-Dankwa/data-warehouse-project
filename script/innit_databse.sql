286e3f4514a4c06a9f0795f6e279781c330f4196
Kwasi Dankwa <104780360+Kwasi-Dankwa@users.noreply.github.com>
2025-06-17 01:33:37 -0400

    Update innit_databse.sql

--- script/innit_databse.sql
+++ script/innit_databse.sql
@@ -13,3 +13,31 @@ WARNING:
     and ensure you have proper backups before running this script.
 */
 
+-- CREATING DATABASE 'WAREHOUSE' --
+
+USE master;
+GO 
+
+-- drop and recreate database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouse;
+
+-- creating database --
+USE DataWarehouse;
+GO
+
+-- creating schemas -- 
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
